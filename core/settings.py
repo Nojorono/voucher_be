72d0fef1e74705080598e526db4fa9aa14002412
BANYU AJI SENJANA <020002618@sonia.net>
2025-07-17 21:52:46 +0700

    fixing settings.py

--- core/settings.py
+++ core/settings.py
@@ -192,8 +192,8 @@ AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')  # Ganti dengan region AWS 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# AWS_DEFAULT_ACL = 'public-read'  # Pastikan file bisa diakses publik
-AWS_DEFAULT_ACL = None  # Pastikan file bisa diakses publik
+AWS_DEFAULT_ACL = 'public-read'  # Pastikan file bisa diakses publik
+# AWS_DEFAULT_ACL = None  # Pastikan file bisa diakses publik
 AWS_S3_FILE_OVERWRITE = False  # Hindari file dengan nama yang sama ditimpa
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}  # Cache selama 1 hari
 
